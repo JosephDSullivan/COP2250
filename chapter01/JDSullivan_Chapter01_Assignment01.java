3a11eac95a9c6d8b78f4c63126147373335bfbc2
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-01-16 20:28:05 -0500

    Chapter 1, assignment 1

---
+++ chapter01/JDSullivan_Chapter01_Assignment01.java
@@ -0,0 +1,28 @@
+/**
+ * @author  Joseph Sullivan
+ * @version 1.0
+ * @since   2025-01-16
+ *
+ */
+package chapter01;
+
+/**
+ * This is a program for calculating and displaying the area and perimeter
+ * of a rectangle with a width of 4.5 and a height of 7.9.
+ */
+public class JDSullivan_Chapter01_Assignment01 {
+    /**
+     * This is the main method.
+     * @param args To be determined
+     */
+    public static void main(String[] args) {
+        float width = 4.5f;
+        float height = 7.9f;
+        float area = width * height;
+        float perimeter = 2 * (width + height);
+        System.out.println("Rectangle width:     " + width);
+        System.out.println("Rectangle height:    " + height);
+        System.out.println("Rectangle area:      " + area);
+        System.out.println("Rectangle perimeter: " + perimeter);
+    }
+}
