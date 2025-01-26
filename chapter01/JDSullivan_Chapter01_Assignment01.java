8005b91c0fbc233233458f1af23c0b1dce42aaa3
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-01-25 22:41:56 -0500

    Minor format change and added comments per instructor notes.

--- chapter01/JDSullivan_Chapter01_Assignment01.java
+++ chapter01/JDSullivan_Chapter01_Assignment01.java
@@ -1,14 +1,13 @@
 /**
  * This is code for chapter 01, assignment 01:
- *      Write a program that displays the area and perimeter of a rectangle
- *      with a width of 4.5 and a height of 7.9 using the following formula:
- *      area = width x height
- *      perimeter = 2 x (width + height)
+ * Write a program that displays the area and perimeter of a rectangle
+ * with a width of 4.5 and a height of 7.9 using the following formula:
+ * area = width x height
+ * perimeter = 2 x (width + height)
  *
- * @author  Joseph Sullivan
+ * @author Joseph Sullivan
  * @version 1.0
- * @since   2025-01-16
- *
+ * @since 2025-01-16
  */
 package chapter01;
 
@@ -22,10 +21,13 @@ public class JDSullivan_Chapter01_Assignment01 {
      * @param args To be determined
      */
     public static void main(String[] args) {
+        //  Store width and height.
         float width = 4.5f;
         float height = 7.9f;
+        //  Calculate area and perimeter.
         float area = width * height;
         float perimeter = 2 * (width + height);
+        //  Display results.
         System.out.println("Rectangle width:     " + width);
         System.out.println("Rectangle height:    " + height);
         System.out.println("Rectangle area:      " + area);
