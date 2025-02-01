531d90af15b67c9fea0886d466a2d128d6674b1c
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-01-31 21:04:43 -0500

    Initialize chapter 03.

---
+++ chapter03/JDSullivan_Chapter03_Assignment17.java
@@ -0,0 +1,35 @@
+/**
+ * Chapter 03, Assignment 17.
+ * Write a program that plays the popular rock-paper-scissor game. (A scissor
+ * can cut a paper, a rock can knock a scissor, and a paper can wrap a rock).
+ * The program randomly generates a number 0, 1, or 2 representing scissor,
+ * rock and paper. The program prompts the user to enter a number 0, 1, or 2
+ * and displays a message indicating whether the user or the computer wins,
+ * loses or draws.
+ * ***Look in the book for a sample run.
+ *
+ * @author Joseph D Sullivan
+ * @version 1.0
+ * @since 2025-01-31
+ */
+package chapter03;
+
+import java.util.Scanner;
+
+/**
+ * This code is for chapter 03, assignment 17.
+ */
+public class JDSullivan_Chapter03_Assignment17 {
+    /**
+     * Play rock, paper, scissors with the user for one round.
+     *
+     * @param args Not processed.
+     */
+    public static void main(String[] args) {
+        //  Create Scanner.
+        Scanner input = new Scanner(System.in);
+
+        //  Obtain user guess.
+        System.out.print("");
+    }
+}
