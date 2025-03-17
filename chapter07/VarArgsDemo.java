fc2e7d0662288a83fe1d6d86ee3e1717d7a03e1f
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-03-16 20:07:49 -0400

    Correction back to book.

--- chapter07/VarArgsDemo.java
+++ chapter07/VarArgsDemo.java
@@ -3,7 +3,7 @@ package chapter07;
 public class VarArgsDemo {
     public static void main(String[] args) {
         printMax(34, 3, 3, 2, 56.5);
-        printMax(1, 2, 3);
+        printMax(new double[]{1, 2, 3});
     }
 
     public static void printMax(double... numbers) {
